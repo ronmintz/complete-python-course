0da4882e3dc6ffde90f9a892661e1faeed74372c
ronmintz <36225693+ronmintz@users.noreply.github.com>
2026-05-13 02:32:47 -0400

    movie exercise

--- course_contents/3_first_milestone_project/milestone_1/incomplete_app.py
+++ course_contents/3_first_milestone_project/milestone_1/incomplete_app.py
@@ -5,31 +5,56 @@ movies = []
 
 
 # You may want to create a function for this code
-title = input("Enter the movie title: ")
-director = input("Enter the movie director: ")
-year = input("Enter the movie release year: ")
-
-movies.append({
-    'title': title,
-    'director': director,
-    'year': year
-})
+def add_movie():
+    title = input("Enter the movie title: ")
+    director = input("Enter the movie director: ")
+    year = input("Enter the movie release year: ")
+    
+    movies.append({
+        'title': title,
+        'director': director,
+        'year': year
+    })
 
 
 # Create other functions for:
+
+def print_movie(m):
+    print(m['title'])
+    print(m['director'])
+    print(m['year'])
+
+    
 #   - listing movies
+def list_movies():
+    for m in movies:
+        print_movie(m)
+
 #   - finding movies
+def find_movies(criterion, value):
+    for m in movies:
+        if criterion == 'title'
+            if m['title'] == value
+                print_movie(m)
+        elif criterion == 'director'
+            if m['director'] == value
+                print_movie(m)
+        elif criterion == 'year'
+            if m['year'] = value
+                print_movie(m)
 
 
 # And another function here for the user menu
 selection = input(MENU_PROMPT)
 while selection != 'q':
     if selection == "a":
-        pass
+        add_movie()
     elif selection == "l":
-        pass
+        list_movies();
     elif selection == "f":
-        pass
+        criterion = input('enter criterion: ')
+        value = input('enter value to select movie: ')
+        find_movies(criterion, value)
     else:
         print('Unknown command. Please try again.')
 
